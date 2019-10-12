209cb6e66e1a5d17a0eb02d27fb836b36d9383f1
KETAN <ketanrajupsc@gamil.com>
2019-10-12 06:37:21 +0530

    the code

---
+++ qwer/code.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"enter the first operand";
+    cin>>a;
+    cout<<"enter the second operand";
+    cin>>b;
+    char d;
+    cout<<"enter the operator";
+    cin>>d;
+    switch(d)
+    {
+        case '+':c=a+b;
+                 break;
+        case '-':c=a-b;
+                 break;
+        case '*':c=a*b;
+                 break;
+        case '/':c=a/b;
+                 break;
+    }
+    cout<<"The result is"<<c;
+    return 0;
+}
+
+
